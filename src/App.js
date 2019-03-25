bee6b99f272c477ba278c5dd8095687b6dcf7df3
David Bolin <david@bolin.at>
2019-03-25 13:32:35 -0700

    created book list from search

--- src/App.js
+++ src/App.js
@@ -7,16 +7,56 @@ class App extends Component {
 
   state = {
     books: [],
-    searchTerm: 'abc',
+    searchTerm: '',
     printType: 'all',
     filter: 'ebooks',
     error: null,
-    loading: false
+    loading: false,
   }
+
+  handleError(e) {
+    console.log(e);
+    this.setState({
+      books: [],
+      error: e
+    })
+  }
+
+  generateBookArr(obj) {
+    const newArr = obj.items.map((item, index) => {
+      const newBookObj = {};
+      newBookObj.name = ((obj.items[index] || {}).volumeInfo || {}).title;
+      newBookObj.picture = (((obj.items[index] || {}).volumeInfo).imageLinks || {}).thumbnail;
+      newBookObj.author = (obj.items[index] || {}).authors;
+      newBookObj.price = ((obj.items[index] || {}).saleInfo || {}).listPrice;
+      newBookObj.description = ((obj.items[index] || {}).searchInfo || {}).textSnippet;
+      return newBookObj;
+    });
+    this.setState({
+      books: newArr
+    })
+    console.log(newArr);
+  }
+    
 
   handleSearch = (e)=> {
     e.preventDefault();
-    console.log(e)
+    const apiKey = 'AIzaSyAjXIfc-Z_K4owIljORps8JiiUEf82UKD4';
+    console.log('started search function');
+    const {searchTerm, printType, filter} = this.state;
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.searchTerm}&filter=${this.state.filter}&printType=${this.state.printType}&key=${apiKey}`)
+      .then(res => res.json())
+      .then(res => {console.log(res);
+        this.generateBookArr(res)})
+      .catch(e => this.handleError(e))
+
+//name  --  res.items[0].volumeInfo.title
+//picture -- res.items[0].volumeInfo.imageLinks.thumbnail (http address str)
+//author -- res.items[0].authors[0]
+//price -- res.items[0].saleInfo.listPrice {amount: number, currencyCode: str}
+//short description -- res.items[0].searchInfo.textSnippet
+
+
   }; 
 
   updateSearchTerm = (term) => {
