b1cf00acdd623f43d58385621a0eddeac0d488f9
likejackonfire <gravitybringsmedown@gmail.com>
2019-03-25 11:27:00 -0700

    fixes on Search.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import Search from './components/Search';
 import BookList from './components/BookList';
 
 class App extends Component {
+
+  state = {
+    books: [],
+    searchTerm: '',
+    error: null,
+    loading: false
+  }
+
+  handleSearch = (e)=> {
+    e.preventDefault();
+    console.log(e)
+  }; 
+
+
   render() {
     return (
       <div className="App">
        <Header />
-       <Search />
+       <Search handleSearch={this.handleSearch}/>
        <BookList />
       </div>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -5,20 +5,20 @@ export default class Search extends React.Component {
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={(e)=> {this.props.handleSearch(e)}}>
           <div className="searchParams searchTerm">
-            <label for="searchBox">Search:</label>
+            <label htmlFor="searchBox">Search:</label>
             <input type="text" id="searchBox"></input>
             <button type="submit">Search</button>
           </div>
           <div className="searchParams searchFilters">
-            <label for="printType">Print Type:</label>
+            <label htmlFor="printType">Print Type:</label>
             <select id="printType">
                <option value="all">All</option>
                <option value="books">Books</option>
                <option value="magazines">Magazines</option>
             </select>
-            <label for="bookType">Book Type:</label>
+            <label htmlFor="bookType">Book Type:</label>
             <select id="bookType">
                <option value="ebooks">All eBooks</option>
                <option value="free-ebooks">Free eBooks</option>
