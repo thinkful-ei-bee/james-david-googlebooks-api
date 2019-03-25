a1f5de068b387bcd0c05c0adfbfea05c9ea1fa3f
likejackonfire <gravitybringsmedown@gmail.com>
2019-03-25 16:44:25 -0700

    adjusted the BookList.js error

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,19 +5,19 @@ export default class BookList extends React.Component {
     produceBook (book) {
         const name = <h2>{book.name}</h2>
         const picture = (
-        <div class="thumbnail">
+        <div className="thumbnail">
           {book.picture ? <img src={book.picture} alt="book thumbnail"></img> : ''}
         </div>
         );
-        const author = <div class="authorName">
+        const author = <div className="authorName">
                          <h3>{book.author ? book.author[0] : ''} </h3>
                        </div>
 
-        const price = <div class="price">
+        const price = <div className="price">
                          <h3>{book.price ? `Price: ${book.price.amount.toLocaleString('en-US', {style: 'currency', currency: book.price.currencyCode})}` : ''}</h3>
                       </div>
 
-        const description = <div class="description">
+        const description = <div className="description">
                               <h3>{ book.description ? book.description : ''}</h3> 
                             </div>
         return <div>
